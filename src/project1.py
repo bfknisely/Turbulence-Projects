220da25f787b1dfb1415922cc449ef75f3b75d05
bfknisely <35206584+bfknisely@users.noreply.github.com>
2018-02-05 21:22:44 -0500

    Initialized U array and applied BCs

--- src/project1.py
+++ src/project1.py
@@ -22,17 +22,70 @@ a uniform grid and central differencing scheme that is fourth-order in y.
 import numpy as np
 import matplotlib.pyplot as plt
 
-# %% Initialize grid
+# %% Initialize uniform grid
 
 Nx = 21  # number of nodes in x-direction
-Ny = 21  # number of nodes in y-direction
+Ny = 11  # number of nodes in y-direction
 
+# Make linear-spaced 1D array of x-values from 0 to 1 with Nx elements
 x = np.linspace(0, 1, Nx)
+
+# Make linear-spaced 1D array of y-values from 0 to 1 with Ny elements
 y = np.linspace(0, 1, Ny)
 
+# Calculate spacings Δx and Δy; these are constant with the uniform grid
+dx = x[1] - x[0]
+dy = y[1] - y[0]
+
+# Initialize U-array: 2D array of zeros of dimension [Nx columns] by [Ny rows]
 u = np.zeros([Ny, Nx])
 
-xx, yy = np.meshgrid(x, y, sparse=True)
-z = xx + yy
+# %% Apply boundary conditions to U matrix
 
-plt.contourf(x,y,z)
+# Set u(x, 0) to be 0 from Dirichlet boundary condition
+u[0, :] = 0  # Set 0th row, all columns to be 0
+
+# Set u(x, 1) to be 1 from Dirichlet boundary condition
+u[-1, :] = 1  # Set last row, all columns to be 1
+
+# Set u(0, y) to be y from Dirichlet boundary condition
+u[:, 0] = y  # Set all rows, 0th column to be y
+
+
+# %% Set up matrix for internal nodes
+
+# Initialize matrix A for equation [[A]]*[u] = [b]
+A = np.zeros([Ny - 2, Ny - 2])
+# Y-dimension reduced by two because u(x, 0) and u(x, 1) are known already
+
+# Use 2nd-Order-Accurate scheme at j = Ny-1 and j = 2
+i = 0
+j = 0
+
+# Use 4th-Order-Accurate scheme for j = 3 to j = Ny - 2
+
+
+# %% Use LU Decomposition matrix solver (Thomas algorithm)
+
+
+# %% Display results spatially
+
+#    z = xx**2.5 + yy**2.5
+plt.figure(figsize=(6, 4))
+plt.contourf(x, y, u, cmap='inferno')
+cbar = plt.colorbar()
+xlab = 'x'
+ylab = 'y'
+fs = 17  # Define font size for figures
+fn = 'Calibri'  # Define font for figures
+figFileName = 'fig1.png'
+plt.xlabel(xlab, fontsize=fs, fontname=fn, fontweight='bold')
+plt.ylabel(ylab + '     ', fontsize=fs, rotation=0, fontname=fn,
+           fontweight='bold')
+plt.xticks(fontsize=fs-5, fontname=fn, fontweight='bold')
+plt.yticks(fontsize=fs-5, fontname=fn, fontweight='bold')
+
+cbar.ax.set_ylabel('u', rotation=0, fontname=fn, fontsize=fs, weight='bold')
+cbar.ax.ticks(fontweight='bold')
+#cbar.set_label('     u', rotation=0, fontsize=fs, fontname=fn,
+#               fontweight='bold')
