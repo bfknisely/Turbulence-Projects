be97fb267f5a29fa7934661ffe406b5f317b4e48
bfknisely <35206584+bfknisely@users.noreply.github.com>
2018-02-17 23:11:55 -0500

    Added plotting error

--- src/project1.py
+++ src/project1.py
@@ -37,13 +37,13 @@ def analytic(Nx, Ny):  # Define function to output analytic solution
     # of dimension [Nx columns] by [Ny rows]
     u = np.zeros([Ny, Nx])
 
-    nTerms = 1  # choose number of terms for infinite sum in solution
+    nTerms = 100  # choose number of terms for infinite sum in solution
 
     for i in range(len(x)):
         for j in range(len(y)):
             v = 0
             for n in range(1, nTerms+1):
-                Bn = -2/(n*pi)**3
+                Bn = 2*(-1)**n / (n*pi)**3
                 v += Bn * sin(n*pi*y[j]) * exp(-(n*pi)**2*x[i])
             U = -y[j]**3/6 + 7*y[j]/6
             u[j, i] = U + v
@@ -130,21 +130,21 @@ def main(Nx, Ny, method):  # Define main function to set up grid and A matrix
     dx = x[1] - x[0]
     dy = y[1] - y[0]
 
-    # Initialize U-array: 2D array of zeros
+    # Initialize u-array: 2D array of zeros
     # of dimension [Nx columns] by [Ny rows]
-    U = np.zeros([Ny, Nx])
-    # capital U is a 2D array of velocities at all spatial locations
+    u = np.zeros([Ny, Nx])
+    # u is a 2D array of velocities at all spatial locations
 
-    # Apply boundary conditions to U matrix
+    # Apply boundary conditions to u matrix
 
-    # Set U(x, 0) to be 0 from Dirichlet boundary condition
-    U[0, :] = 0  # Set 0th row, all columns to be 0
+    # Set u(x, 0) to be 0 from Dirichlet boundary condition
+    u[0, :] = 0  # Set 0th row, all columns to be 0
 
-    # Set U(x, 1) to be 1 from Dirichlet boundary condition
-    U[-1, :] = 1  # Set last row, all columns to be 1
+    # Set u(x, 1) to be 1 from Dirichlet boundary condition
+    u[-1, :] = 1  # Set last row, all columns to be 1
 
-    # Set U(0, y) to be y from Dirichlet boundary condition
-    U[:, 0] = y  # Set all rows, 0th column to be y
+    # Set u(0, y) to be y from Dirichlet boundary condition
+    u[:, 0] = y  # Set all rows, 0th column to be y
 
     for i in range(len(x)-1):
 
@@ -163,16 +163,16 @@ def main(Nx, Ny, method):  # Define main function to set up grid and A matrix
         A[0, 1] = -1/(2*dy**2)  # Assign value in matrix location [1, 2]
 
         # Assign right-hand side of equation (known values) for 0th value in b
-        b[0] = (y[1] + 1/(2*dy**2)*U[0, i] + (-1/dy**2 + 1/dx)*U[1, i]
-                + 1/(2*dy**2)*U[2, i] + 1/(2*dy**2)*U[0, i+1])
+        b[0] = (y[1] + 1/(2*dy**2)*u[0, i] + (-1/dy**2 + 1/dx)*u[1, i]
+                + 1/(2*dy**2)*u[2, i] + 1/(2*dy**2)*u[0, i+1])
 
         # at j = Ny-2 (near top border of domain)
         A[-1, -1] = 1/dx + 1/dy**2  # Assign value to last diagonal element
         A[-1, -2] = -1/(2*dy**2)  # Assign value to left of last diag element
 
         # Assign right-hand side of equation (known values) for last value in b
-        b[-1] = (y[-2] + 1/(2*dy**2)*U[-3, i] + (-1/dy**2 + 1/dx)*U[-2, i]
-                 + 1/(2*dy**2)*U[-1, i] + 1/(2*dy**2)*U[-1, i+1])
+        b[-1] = (y[-2] + 1/(2*dy**2)*u[-3, i] + (-1/dy**2 + 1/dx)*u[-2, i]
+                 + 1/(2*dy**2)*u[-1, i] + 1/(2*dy**2)*u[-1, i+1])
 
         # # # Use 4th-Order-Accurate scheme for j = 2 to j = Ny-3
 
@@ -181,18 +181,18 @@ def main(Nx, Ny, method):  # Define main function to set up grid and A matrix
         A[1, 2] = -2/(3*dy**2)
         A[1, 3] = 1/(24*dy**2)
 
-        b[1] = (y[2] + -1/(24*dy**2)*U[0, i] + 2/(3*dy**2)*U[1, i]
-                + (1/dx-5/(4*dy**2))*U[2, i] + 2/(3*dy**2)*U[3, i]
-                + (-1/(24*dy**2))*U[4, i] + (-1/(24*dy**2))*U[0, i+1])
+        b[1] = (y[2] + -1/(24*dy**2)*u[0, i] + 2/(3*dy**2)*u[1, i]
+                + (1/dx-5/(4*dy**2))*u[2, i] + 2/(3*dy**2)*u[3, i]
+                + (-1/(24*dy**2))*u[4, i] + (-1/(24*dy**2))*u[0, i+1])
 
         A[-2, -1] = -2/(3*dy**2)
         A[-2, -2] = 5/(4*dy**2) + 1/dx
         A[-2, -3] = -2/(3*dy**2)
         A[-2, -4] = 1/(24*dy**2)
 
-        b[-2] = (y[-3] + -1/(24*dy**2)*U[-5, i] + 2/(3*dy**2)*U[-4, i]
-                 + (1/dx-5/(4*dy**2))*U[-3, i] + 2/(3*dy**2)*U[-2, i]
-                 + (-1/(24*dy**2))*U[-1, i] + (-1/(24*dy**2))*U[-1, i+1])
+        b[-2] = (y[-3] + -1/(24*dy**2)*u[-5, i] + 2/(3*dy**2)*u[-4, i]
+                 + (1/dx-5/(4*dy**2))*u[-3, i] + 2/(3*dy**2)*u[-2, i]
+                 + (-1/(24*dy**2))*u[-1, i] + (-1/(24*dy**2))*u[-1, i+1])
 
         for j in range(2, Ny-4):
             A[j, j-2] = 1/(24*dy**2)
@@ -200,33 +200,33 @@ def main(Nx, Ny, method):  # Define main function to set up grid and A matrix
             A[j, j] = 5/(4*dy**2) + 1/dx
             A[j, j+1] = -2/(3*dy**2)
             A[j, j+2] = 1/(24*dy**2)
-            b[j] = (y[j+1] + -1/(24*dy**2)*U[j-1, i] + 2/(3*dy**2)*U[j, i]
-                    + (1/dx-5/(4*dy**2))*U[j+1, i] + 2/(3*dy**2)*U[j+2, i]
-                    + (-1/(24*dy**2))*U[j+3, i])
+            b[j] = (y[j+1] + -1/(24*dy**2)*u[j-1, i] + 2/(3*dy**2)*u[j, i]
+                    + (1/dx-5/(4*dy**2))*u[j+1, i] + 2/(3*dy**2)*u[j+2, i]
+                    + (-1/(24*dy**2))*u[j+3, i])
 
         # x_ans = (inv(A)@b).transpose()  # Use built-in inverse operator
         if method == 'lu':
-            U[1:-1, i+1] = pentaLU(A, b)
+            u[1:-1, i+1] = pentaLU(A, b)
         if method == 'inv':
-            U[1:-1, i+1] = (inv(A)@b).transpose()
+            u[1:-1, i+1] = (inv(A)@b).transpose()
 
-    return U
+    return u
     # End of main function
 
 
-def makePlots(U):  # Display results spatially
+def makePlots(u):  # Display results spatially
 
     # Form x and y arrays
-    Ny, Nx = np.shape(U)
+    Ny, Nx = np.shape(u)
     # Make linear-spaced 1D array of x-values from 0 to 1 with Nx elements
     x = np.linspace(0, 1, Nx)
     # Make linear-spaced 1D array of y-values from 0 to 1 with Ny elements
     y = np.linspace(0, 1, Ny)
 
-    # Create contour plot of U vs x and y
+    # Create contour plot of u vs x and y
     plt.figure(figsize=(6, 4))
-    plt.contourf(x, y, U, cmap='plasma', levels=np.linspace(0., 1., 11))
-    # plt.contourf(x, y, U, cmap='plasma')
+    plt.contourf(x, y, u, cmap='plasma', levels=np.linspace(0., 1., 11))
+    # plt.contourf(x, y, u, cmap='plasma')
     cbar = plt.colorbar()
     fs = 17  # Define font size for figures
     fn = 'Calibri'  # Define font for figures
@@ -250,7 +250,7 @@ def makePlots(U):  # Display results spatially
     plt.figure()  # create new figure
     for n in range(len(xLocs)):
         col = np.argmin(abs(x-xLocs[n]))  # extract value closest to given xLoc
-        plt.plot(U[:, col], y, lines[n])
+        plt.plot(u[:, col], y, lines[n])
         legStr.append('x = {}'.format(xLocs[n]))
 
     plt.legend(legStr, fontsize=fs-2)
@@ -261,13 +261,62 @@ def makePlots(U):  # Display results spatially
     plt.yticks(fontsize=fs-2, fontname=fn, fontweight='bold')
 
 
+def errorPlots(u, u_an):
+    # Form x and y arrays
+    Ny, Nx = np.shape(u)
+    # Make linear-spaced 1D array of x-values from 0 to 1 with Nx elements
+    x = np.linspace(0, 1, Nx)
+    # Make linear-spaced 1D array of y-values from 0 to 1 with Ny elements
+    y = np.linspace(0, 1, Ny)
+    # Create contour plot of error (u-u_an) vs x and y
+    plt.figure(figsize=(6, 4))
+    cmax = round(np.amax(abs(u-u_an)), 3)  # maximum absolute contour value
+    lev = np.linspace(0, cmax, 11)
+    plt.contourf(x, y, abs(u-u_an), cmap='plasma',
+                 levels=lev)
+    cbar = plt.colorbar()
+    fs = 17  # Define font size for figures
+    fn = 'Calibri'  # Define font for figures
+    plt.xlabel('x', fontsize=fs, fontname=fn, fontweight='bold')
+    plt.ylabel('y' + '     ', fontsize=fs, rotation=0, fontname=fn,
+               fontweight='bold')
+    plt.xticks(fontsize=fs-2, fontname=fn, fontweight='bold')
+    plt.yticks(fontsize=fs-2, fontname=fn, fontweight='bold')
+    cbar.ax.set_ylabel('                   |${u-u_{an}}$|', rotation=0,
+                       fontname=fn, fontsize=fs, weight='bold')
+    #    cbar.ax.set_yticklabels(cbar.get_ticks(),
+    #                            fontsize=fs-2, fontname=fn, weight='bold')
+    cbar.ax.set_yticklabels([round(cbar.get_ticks()[n], 4)
+                            for n in range(len(cbar.get_ticks()))],
+                            fontsize=fs-2, fontname=fn, weight='bold')
+
+    # Look at error in solution at selected x-locations
+    xLocs = [0.05, 0.1, 0.2, 0.5, 1.0]
+    lines = ['y-', 'r-.', 'm--', 'b:', 'k-']
+    # Loop for each x-location to make plots at each location comparing exact
+    # solution with analytical solution
+    legStr = []
+    plt.figure()  # create new figure
+    for n in range(len(xLocs)):
+        col = np.argmin(abs(x-xLocs[n]))  # extract value closest to given xLoc
+        plt.plot(abs(u[:, col]-u_an[:, col]), y, lines[n])
+        legStr.append('x = {}'.format(xLocs[n]))
+
+    plt.legend(legStr, fontsize=fs-2)
+    plt.xlabel('|${u-u_{an}}$|', fontsize=fs, fontname=fn, fontweight='bold')
+    plt.ylabel('y' + '     ', fontsize=fs, rotation=0, fontname=fn,
+               fontweight='bold')
+    plt.xticks(fontsize=fs-2, fontname=fn, fontweight='bold')
+    plt.yticks(fontsize=fs-2, fontname=fn, fontweight='bold')
+
+
 # Run functions in order
 t0 = time.time()
 Nx = 21
-Ny = 41
-U = main(Nx, Ny, 'lu')
+Ny = 21
+u = main(Nx, Ny, 'lu')
 u_an = analytic(Nx, Ny)
+makePlots(u)
+errorPlots(u, u_an)
 elapsed = time.time() - t0
 print('Elapsed time is ' + '{0:0.4}'.format(elapsed) + ' s.')
-makePlots(U)
-makePlots(u_an)
